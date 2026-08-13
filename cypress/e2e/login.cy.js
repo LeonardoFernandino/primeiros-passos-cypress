3614b3e56035329655893dce889ff6a6cac4f552
Leonardo Fernandino <leonardoafernandino@gmail.com>
2026-08-13 14:11:11 -0300

    Added userData

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,19 +8,30 @@ describe('Orange HRM Tests', () => {
     dashboardGrid: '.orangehrm-dashboard-grid',
     wrongCredentialAlert: '[role="alert"]'
   }
+
+  const userData = {
+    userSuccess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'Teste',
+      password: 'teste123'
+    }
+  }
   
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Teste')
-    cy.get(selectorList.passwordField).type('teste123')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert).should('contain', 'Invalid credentials')
   })
